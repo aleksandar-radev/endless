dc09fc2f64b1d2d819458f265115e2bd95c53107
Aleksandar Radev <alexander.radev.96@gmail.com>
2025-07-04 16:17:14 +0300

    0.2.1

--- src/options.js
+++ src/options.js
@@ -8,7 +8,7 @@ const html = String.raw;
 // Options class to store options and version (future-proof for migrations)
 export class Options {
   constructor(data = {}) {
-    this.version = data.version || '0.2.0';
+    this.version = data.version || '0.2.1';
     // Add startingStage, default to null (unset)
     this.startingStage = data.startingStage || null;
     // Add showEnemyStats option, default to false
