73cf27db3cd3188270bac9706c6f5ee5134be6ef
Aleksandar Radev <36519809+aleksandar-radev@users.noreply.github.com>
2025-08-11 02:06:46 +0300

    Bump version to 0.7.2 and update changelog with new features and improvements

---
+++ src/changelog/0.7.2.js
@@ -0,0 +1,12 @@
+const html = String.raw;
+export default function run() {
+  return html`
+  <span style="color:#4F8A10;">📝 2025-08-11</span>
+  <hr>
+
+  <span style="color:#FFA500;">✨ Features & Improvements</span>
+  <ul>
+    <li>Fix visual issue for item tooltips</li>
+  </ul>
+  `;
+}
