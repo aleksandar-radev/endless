25b02417e2f9790c1a3fdd945429721cf9ff93d1
Aleksandar Radev <36519809+aleksandar-radev@users.noreply.github.com>
2025-08-10 19:02:25 +0300

    Fix resetRequired option handling in migration scripts to prevent unnecessary updates

--- src/migrations/0.6.0.js
+++ src/migrations/0.6.0.js
@@ -2,7 +2,10 @@ export function run(rawData) {
   let data = JSON.parse(JSON.stringify(rawData || {}));
 
   // leaderboard removed anyway
-  data.options.resetRequired = false;
+
+  if (data.options?.resetRequired) {
+    data.options.resetRequired = false;
+  }
 
   return {
     data,
