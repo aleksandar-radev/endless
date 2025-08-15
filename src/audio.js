922916c215454971101b389f50925860ce4cf63d
Aleksandar Radev <36519809+aleksandar-radev@users.noreply.github.com>
2025-08-16 02:23:36 +0300

    refactor audio manager to use scaled volume for sound playback

--- src/audio.js
+++ src/audio.js
@@ -7,12 +7,13 @@ const SOUNDS = {
 
 class AudioManager {
   constructor() {
-    this.volume = 0; // Default volume (0.0 - 1.0)
+    this.volume = 0; // Slider value (0.0 - 1.0)
+    this.scaledVolume = 0; // Actual playback volume after scaling
     this.sounds = {};
     // Preload all sounds
     for (const [key, src] of Object.entries(SOUNDS)) {
       const audio = new Audio(src);
-      audio.volume = this.volume;
+      audio.volume = this.scaledVolume;
       this.sounds[key] = audio;
     }
   }
@@ -20,18 +21,19 @@ class AudioManager {
   setVolume(vol) {
     // Use cubic scaling for even steeper perceived loudness curve
     this.volume = Math.max(0, Math.min(1, vol));
-    const scaled = Math.pow(this.volume, 3);
+    this.scaledVolume = Math.pow(this.volume, 3);
     for (const audio of Object.values(this.sounds)) {
-      audio.volume = scaled;
+      audio.volume = this.scaledVolume;
     }
   }
 
   play(key) {
+    if (this.scaledVolume <= 0) return;
     const audio = this.sounds[key];
     if (audio) {
       // Clone to allow overlapping sounds
       const clone = audio.cloneNode();
-      clone.volume = this.volume;
+      clone.volume = this.scaledVolume;
       clone.play();
     }
   }
