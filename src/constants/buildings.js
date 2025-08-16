910c5d2bf510c407de53a959405f741f42d6c8a2
Aleksandar Radev <36519809+aleksandar-radev@users.noreply.github.com>
2025-08-16 18:22:06 +0300

    reduce buildings costs a little

--- src/constants/buildings.js
+++ src/constants/buildings.js
@@ -9,8 +9,8 @@ export const buildingsData = {
     image: '/buildings/crystal-lab.png',
     effect: { type: 'crystal', amount: 1, interval: 'hour' },
     costStructure: {
-      gold: { base: 3000, increment: 150, cap: 15000 },
-      crystal: { base: 8, increment: 1, cap: 20 },
+      gold: { base: 2000, increment: 150, cap: 15000 },
+      crystal: { base: 5, increment: 1, cap: 20 },
     },
     maxLevel: 50000,
     unlockRequirements: {},
@@ -35,8 +35,8 @@ export const buildingsData = {
     effect: { type: 'soul', amount: 5, interval: 'hour' },
     costStructure: {
       gold: { base: 10000, increment: 500, cap: 50000 },
-      crystal: { base: 12, increment: 2, cap: 30 },
-      soul: { base: 16, increment: 2, cap: 50 },
+      crystal: { base: 12, increment: 1, cap: 30 },
+      soul: { base: 10, increment: 2, cap: 50 },
     },
     maxLevel: 30000,
     unlockRequirements: {},
