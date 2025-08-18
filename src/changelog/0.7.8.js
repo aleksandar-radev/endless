08aac6d4f1b3c246627b62c1ac781e39c181d48f
Aleksandar Radev <36519809+aleksandar-radev@users.noreply.github.com>
2025-08-18 15:22:27 +0300

    0.7.8 feat: Bump version to 0.7.8 and update changelog with recent improvements

---
+++ src/changelog/0.7.8.js
@@ -0,0 +1,14 @@
+const html = String.raw;
+export default function run() {
+  return html`
+  <span style="color:#4F8A10;"> 2025-08-18</span>
+  <hr>
+
+  <span style="color:#0078d7;"> Improvements</span>
+  <ul>
+    <li>Fixed training display issue for elemental damages.</li>
+    <li>Fixed a display issue on some browsers in inventory.</li>
+    <li>Fixed a bug with boss fights (Arena).</li>
+  </ul>
+  `;
+}
--- src/changelog/changelog.js
+++ src/changelog/changelog.js
@@ -27,6 +27,7 @@ import c0_7_4 from './0.7.4.js';
 import c0_7_5 from './0.7.5.js';
 import c0_7_6 from './0.7.6.js';
 import c0_7_7 from './0.7.7.js';
+import c0_7_8 from './0.7.8.js';
 
 export const CHANGELOG = {
   '0.0.1': {
@@ -144,5 +145,9 @@ export const CHANGELOG = {
   '0.7.7': {
     version: '0.7.7',
     run: c0_7_7,
+  },
+  '0.7.8': {
+    version: '0.7.8',
+    run: c0_7_8,
   },
 };
