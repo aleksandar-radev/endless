1e7feb154c97bf8c7959c2128f048d06f2db2f70
Aleksandar Radev <36519809+aleksandar-radev@users.noreply.github.com>
2025-09-19 19:18:43 +0300

    feat: update version to 0.8.0 in package.json and options.js

--- src/options.js
+++ src/options.js
@@ -85,7 +85,7 @@ function attachTooltip(el, key, ...params) {
 // Options class to store options and version (future-proof for migrations)
 export class Options {
   constructor(data = {}) {
-    this.version = data.version || '0.7.14';
+    this.version = data.version || '0.8.0';
     // Add startingStage, default to null (unset)
     this.startingStage = data.startingStage || null;
     // Add showEnemyStats option, default to false
