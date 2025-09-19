20922b15d452e5059200b1a90a99ee1f707ac0f4
Aleksandar Radev <36519809+aleksandar-radev@users.noreply.github.com>
2025-09-19 22:50:49 +0300

    v0.8.3

--- src/changelog/0.8.3.js
+++ src/changelog/0.8.3.js
@@ -4,7 +4,7 @@ export default function run() {
     <span style="color:#4F8A10;"> 2025-09-19</span>
     <hr>
 
-    <span style="color:#FF8A00;"> Improvements</span>
+    <span style="color:#FF8A00;"> Fixes</span>
     <ul>
       <li>Prestige no longer resets Ascension points or upgrades; Ascension progress persists across prestiges.</li>
       <li>Ascension now preserves all settings/options (same as Prestige), while still resetting all other progress.</li>
