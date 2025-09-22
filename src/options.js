9b23ae7d7ac5abe50593b685e6112e6ef2026562
Aleksandar Radev <36519809+aleksandar-radev@users.noreply.github.com>
2025-09-22 19:31:53 +0300

    feat: update version to 0.8.6 and add migration script for data handling

--- src/options.js
+++ src/options.js
@@ -89,7 +89,7 @@ function attachTooltip(el, key, ...params) {
 // Options class to store options and version (future-proof for migrations)
 export class Options {
   constructor(data = {}) {
-    this.version = data.version || '0.8.5';
+    this.version = data.version || '0.8.6';
     // Add startingStage, default to null (unset)
     this.startingStage = data.startingStage || null;
     // Add showEnemyStats option, default to false
