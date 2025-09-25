5fa18ff2c0f20da99a1dbb847e89588a5b510187
Aleksandar Radev <36519809+aleksandar-radev@users.noreply.github.com>
2025-09-25 20:19:31 +0300

    Update changelog for version 0.8.8 with new improvements and bug fixes

--- src/changelog/0.8.8.js
+++ src/changelog/0.8.8.js
@@ -4,6 +4,11 @@ export default function run() {
     <span style="color:#4F8A10;">2025-09-26</span>
     <hr>
 
+    <span style="color:#FF8A00;">Improvements</span>
+    <ul>
+      <li>Add different colors for damages based on highest.</li>
+    </ul>
+
     <span style="color:#FF8A00;">Bug Fixes</span>
     <ul>
       <li>Fixed building reward timers when network conditions are poor.</li>
