21a00b48606ea15e2262cbdabed9d85176a23d30
Aleksandar Radev <alexander.radev.96@gmail.com>
2026-02-24 12:30:37 +0200

    v0.9.1

--- src/options.js
+++ src/options.js
@@ -66,7 +66,7 @@ function attachTooltip(el, generator) {
 // Options class to store options and version (future-proof for migrations)
 export class Options {
   constructor(data = {}) {
-    this.version = data.version || '0.9.0';
+    this.version = data.version || '0.9.1';
     this.startingStage = data.startingStage || null;
     this.showEnemyStats = data.showEnemyStats ?? true;
     this.showAllStats = data.showAllStats ?? false;
