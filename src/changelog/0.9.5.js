2aabae9fd934786f085c33bedfecea12a2f5fdfd
Aleksandar Radev <alexander.radev.96@gmail.com>
2026-02-26 09:47:08 +0200

    v0.9.5 Bump version to 0.9.5 and add changelog for bug fixes and migration support

---
+++ src/changelog/0.9.5.js
@@ -0,0 +1,12 @@
+const html = String.raw;
+export default function run() {
+  return html`
+    <span style="color:#4F8A10;">2026-02-26</span>
+    <hr />
+
+    <span style="color:#FF8A00;">Bug Fixes</span>
+    <ul>
+      <li>Fixed login and registration issues in the desktop application.</li>
+    </ul>
+  `;
+}
