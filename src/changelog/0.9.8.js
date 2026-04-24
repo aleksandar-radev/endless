40dcb15a2b699ca53f009fcdf90ee69bfafd4b93
Aleksandar Radev <alexander.radev.96@gmail.com>
2026-04-24 22:21:28 +0300

    Bump version to 0.9.8 and update changelog; add migration script for version handling

--- src/changelog/0.9.8.js
+++ src/changelog/0.9.8.js
@@ -1,7 +1,7 @@
 const html = String.raw;
 export default function run() {
   return html`
-    <span style="color:#4F8A10;">2026-03-20</span>
+    <span style="color:#4F8A10;">2026-04-24</span>
     <hr />
 
     <span style="color:#00529B;">Features</span>
